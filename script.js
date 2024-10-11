ee4bb7a246fe4f6b993bfc876075e1686f946ef6
Kanatip Jaksarn <Kanatip.jak@gmail.com>
2024-10-11 21:30:27 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
+window.onload = function() {
+  // Initialize LIFF
+  liff.init({
+      liffId: "2005012452-og8X4PgJ"
+  }).then(() => {
+      console.log("LIFF initialized successfully");
+  }).catch((err) => {
+      console.error("LIFF initialization failed:", err);
+  });
 
   // Function to submit form data
   document.getElementById('submitData').addEventListener('click', function() {
@@ -26,3 +34,4 @@
   document.getElementById('mapPage').addEventListener('click', function() {
       window.location.href = "https://jeante2549.github.io/Form_Ricehub2/";  // Replace with the URL of your map page
   });
+};
